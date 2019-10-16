2cfaf15f75a501b194ffe4d2b96c0e430a4d9035
procopio matteo <procopio.matteo1@gmail.com>
2019-10-16 17:49:47 +0200

    try to select krakow data

--- krakus/utils.py
+++ krakus/utils.py
@@ -12,6 +12,24 @@ def download (month,year):
         with myzip.open('s_t_01_2019.csv') as myfile:
             print(myfile.read())
 
-    # dezippare contenuto s_t_01_2019.csv dentro a response.zip 
+def filtrfile (origial,new):
+    f1 = open(original, "rb")
+    f2 = open(new, "wb") 
+    for n in original:
+        line = f1.readline()
+        if line == "":
+            break
+        if line[0] == "350190566":
+            continue
+        f2.write(line)
+    f1.close()
+    f2.close()
+    return
 
-    # leggere il contenuto del file CSV e creare un nuovo file CSV con dentro solo i datidi cracovia
+
+
+
+
+    # dezippare contenuto s_t_01_2019.csv dentro a response.zip
+
+    # leggere il contenuto del file CSV e creare un nuovo file CSV con dentro solo i dati di cracovia
